432a9a6834bae7542a276c2f83448dd952e909f5
HowXu <howxu366@outlook.com>
2025-04-15 23:02:55 +0800

    [dev] vue app deving 1

---
+++ book-manager-vue/src/App.vue
@@ -0,0 +1,10 @@
+
+<!-- 主界面 -->
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>SpringBoot + Vue + Mysql</h1>
+</template>
---
+++ book-manager-vue/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// 这里创建app代表这个Vue前端实例 创建的组件是App
+const app = createApp(App)
+// 挂载在id为app的DOM元素上 这个元素表现为一个div 现在这个div就嵌入了App.vue的那个h1标签
+app.mount("#app")
